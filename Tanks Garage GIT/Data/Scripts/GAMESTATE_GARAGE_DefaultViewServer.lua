2eda3a6273c452f9faeb60dcda94946259c5ffe6
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2021-03-14 23:34:49 -0700

    Minor changes to fix a bug that ocured during testing.
    Fixed a bug where the selected tank was not set when the player joined.
    
    
    Former-commit-id: 03c90ecdfddb6dc7f50e4c8d72cb58216c3e50f0

--- Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
+++ Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
@@ -1,9 +1,9 @@
 
 local map = {}
 
-map["FIELD"] = "a558d4/armored-force-map-1test"
-map["DESERT"] = "086c42/armored-force-map-2-test"
-map["TUNDRA"] = "1f57c5/armored-force-map-3-test"
+map["FIELD"] = "9fdb74/armored-force-map-2"
+map["DESERT"] = "9fdb74/armored-force-map-2"
+map["TUNDRA"] = "9fdb74/armored-force-map-2"
 
 function SendToMap(player, selectedMap)
 
