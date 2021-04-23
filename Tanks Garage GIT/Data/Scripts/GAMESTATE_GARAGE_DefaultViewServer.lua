384e060919f679ed1870bdb1be092ebaece2eb98
datonare <datbow85@gmail.com>
2021-04-23 11:50:22 -0400

    Added functionality to deploy maps
    Added functionality to deploy maps in garage UI

--- Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
+++ Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
@@ -4,6 +4,10 @@ local map = {}
 map["LastTeam"] = {"ed1bcb/tanks-map-1-lts", "9fdb74/armored-force-map-2", "1f57c5/armored-force-map-3-test"}
 map["Frontline"] = {"c80e70/tanks-map-1-frt", "24830e/tanks-map-2-frt", "3084a4/tanks-map-3-frt"}
 
+-- Individual maps
+map["Fields"] = {"ed1bcb/tanks-map-1-lts", "c80e70/tanks-map-1-frt"}
+map["Desert"] = {"9fdb74/armored-force-map-2", "24830e/tanks-map-2-frt"}
+map["Tundra"] = {"1f57c5/armored-force-map-3-test", "3084a4/tanks-map-3-frt"}
 
 function SendToMap(player, selectedMap)
 	
