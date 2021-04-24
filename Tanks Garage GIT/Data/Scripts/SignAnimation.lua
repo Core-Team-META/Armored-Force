423fa774f788caa69bcf1698e7cb6f127cd2a5a0
AwkwardGameDev <55956768+AwkwardGameDev@users.noreply.github.com>
2021-04-23 23:05:57 -0500

    garage: changed some fonts and added sign animation

--- Tanks Garage GIT/Data/Scripts/SignAnimation.lua
+++ Tanks Garage GIT/Data/Scripts/SignAnimation.lua
@@ -1,15 +1,11 @@
---[[ local root = script.parent
+local root = script.parent
 local sign = script:GetCustomProperty("sign"):WaitForObject()
 
 local Ease3D = require(script:GetCustomProperty("Ease3D"))
 
 function Tick()
-    Ease3D.EaseRotation(sign, Rotation.New(0, 3.712, 0), 3, Ease3D.EasingEquation.BACK, Ease3D.EasingDirection.INOUT)
-    Task.Wait(1.5)
-    Ease3D.EaseRotation(sign, Rotation.New(0, -11.367, 0), 4, Ease3D.EasingEquation.BACK, Ease3D.EasingDirection.INOUT)
-    Task.Wait(2)
-    Ease3D.EaseRotation(sign, Rotation.New(0, 1.347, 0), 3, Ease3D.EasingEquation.BACK, Ease3D.EasingDirection.INOUT)
-    Task.Wait(1.5)
-    Ease3D.EaseRotation(sign, Rotation.New(0, -6.547, 0), 3, Ease3D.EasingEquation.BACK, Ease3D.EasingDirection.INOUT)
-    Task.Wait(1.5)
-end ]]
+    Ease3D.EaseRotation(sign, Rotation.New(0, -20, 0), 5, Ease3D.EasingEquation.SINE, Ease3D.EasingDirection.INOUT)
+    Task.Wait(0.75)
+    Ease3D.EaseRotation(sign, Rotation.New(0, 20, 0), 5, Ease3D.EasingEquation.SINE, Ease3D.EasingDirection.INOUT)
+    Task.Wait(0.75)
+end
