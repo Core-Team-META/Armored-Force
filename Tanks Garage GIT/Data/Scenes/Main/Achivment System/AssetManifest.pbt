b11eb96857b279e6fb51955f56e0e724284f4290
Brent Dunn <brent075@gmail.com>
2021-04-26 16:31:13 -0700

    Added: Achievement system to garage map

---
+++ Tanks Garage GIT/Data/Scenes/Main/Achivment System/AssetManifest.pbt
@@ -0,0 +1,117 @@
+Assets {
+  Id: 17066449609679557524
+  Name: "Frame Outline Thin 002"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "FrameOutline_019"
+  }
+}
+Assets {
+  Id: 16881924154795479640
+  Name: "Fantasy Ability Blue 006"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Fantasy_Ability_Blue_006"
+  }
+}
+Assets {
+  Id: 16806313330175629846
+  Name: "Frame Outlined Thick 001"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "FrameFlat_020"
+  }
+}
+Assets {
+  Id: 16670222695632479546
+  Name: "Play Bold"
+  PlatformAssetType: 28
+  PrimaryAsset {
+    AssetType: "FontAssetRef"
+    AssetId: "PlayBold_ref"
+  }
+}
+Assets {
+  Id: 14060330023043759337
+  Name: "BG Flat 001"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "BackgroundNoOutline_020"
+  }
+}
+Assets {
+  Id: 11074002944006108218
+  Name: "Fantasy Ability Red 020"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Fantasy_Ability_Red_020"
+  }
+}
+Assets {
+  Id: 10258515349694145501
+  Name: "BG Flat 002"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "BackgroundNoOutline_019"
+  }
+}
+Assets {
+  Id: 10071982093329629272
+  Name: "UI Basic Corners 001"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Basic_Corners_001"
+  }
+}
+Assets {
+  Id: 9084852752205607158
+  Name: "Fantasy Ability Blue 016"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Fantasy_Ability_Blue_016"
+  }
+}
+Assets {
+  Id: 4417804760414966795
+  Name: "Fantasy Ability Green 011"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Fantasy_Ability_Green_011"
+  }
+}
+Assets {
+  Id: 3884726409041590116
+  Name: "Sci-fi Ability Green 025"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_SciFi_Ability_Green_025"
+  }
+}
+Assets {
+  Id: 2604353830693238371
+  Name: "BG Highlighted 001"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "BackgroundHighlighted_020"
+  }
+}
+Assets {
+  Id: 1283463588493558965
+  Name: "Fantasy Gold 001"
+  PlatformAssetType: 9
+  PrimaryAsset {
+    AssetType: "PlatformBrushAssetRef"
+    AssetId: "UI_Fantasy_Equip_Gold_002"
+  }
+}
---
+++ Tanks Garage GIT/Data/Scripts/ACHIEVEMENT_Client.lua
@@ -0,0 +1,209 @@
+------------------------------------------------------------------------------------------------------------------------
+-- Achievement System Client
+-- Author Morticai (META) - (https://www.coregames.com/user/d1073dbcc404405cbef8ce728e53d380)
+-- Date: 2021/4/26
+-- Version 0.1.4-AF
+------------------------------------------------------------------------------------------------------------------------
+local ROOT = script:GetCustomProperty("AchievementSystem"):WaitForObject()
+local isEnabled = ROOT:GetCustomProperty("Enabled")
+------------------------------------------------------------------------------------------------------------------------
+-- REQUIRES
+------------------------------------------------------------------------------------------------------------------------
+local ACH_API = require(script:GetCustomProperty("ACH_API"))
+local EaseUI = require(script:GetCustomProperty("EaseUI"))
+--local ABGS = require(script:GetCustomProperty("APIBasicGameState"))
+------------------------------------------------------------------------------------------------------------------------
+-- OBJECTS
+------------------------------------------------------------------------------------------------------------------------
+local ACHIEVEMENT_LIST = script:GetCustomProperty("Achievement_List"):WaitForObject()
+local NOTIFICATION = script:GetCustomProperty("NOTIFICATION"):WaitForObject()
+local NOTIFICATION_ICON_BG = NOTIFICATION:GetCustomProperty("ICONBG"):WaitForObject()
+local NOTIFICATION_ICON = NOTIFICATION:GetCustomProperty("ICON"):WaitForObject()
+local ACHIEVEMENT_NAME_TEXT = NOTIFICATION:GetCustomProperty("ACHIEVEMENT_NAME_TEXT"):WaitForObject()
+local ACHIEVEMENTS_DETAILS_UI = nil
+ --script:GetCustomProperty("ACHIEVEMENTS_DETAILS_UI"):WaitForObject()
+
+local ACHIEVEMENT_PANEL = nil--script:GetCustomProperty("AchievementsPanel"):WaitForObject()
+
+local LOCAL_PLAYER = Game.GetLocalPlayer()
+
+local SFX = script:GetCustomProperty("SFX")
+local AchievementPanelTemplate = script:GetCustomProperty("Achievement_EndScreen_Template")
+------------------------------------------------------------------------------------------------------------------------
+-- Variables
+------------------------------------------------------------------------------------------------------------------------
+local shouldShow = false
+local achievementQueue = {}
+local achievementIds = {}
+local listeners = {}
+local scriptListeners = {}
+
+NOTIFICATION.visibility = Visibility.FORCE_OFF
+------------------------------------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------
+local function BuildIdTable()
+    for _, achievement in pairs(ACH_API.GetAchievements()) do
+        achievementIds[achievement.sort] = achievement.id
+    end
+end
+
+local function IsAchievement(id)
+    for _, achievementId in pairs(achievementIds) do
+        if id == achievementId then
+            return true
+        end
+    end
+    return false
+end
+
+local function ClearListeners(listeners)
+    for _, listener in ipairs(listeners) do
+        if listener and listener.isConnected then
+            listeners:Disconnect()
+        end
+    end
+    listeners = {}
+end
+
+local function ClearAchievements()
+    if not ACHIEVEMENT_PANEL then return end
+    for _, child in ipairs(ACHIEVEMENT_PANEL:GetChildren()) do
+        if Object.IsValid(child) then
+            child:Destroy()
+        end
+    end
+end
+
+local function OnDetailsHover(button)
+    if not button.clientUserData.detailPanel then
+        return
+    end
+    local achievement = button.clientUserData.achievement
+    ACHIEVEMENTS_DETAILS_UI.visibility = Visibility.FORCE_OFF
+    if (iconBG) then
+        ACHIEVEMENTS_DETAILS_UI:GetCustomProperty("ACHIEVEMENT_ICON_BG"):WaitForObject():SetImage(achievement.iconBG)
+    end
+    ACHIEVEMENTS_DETAILS_UI:GetCustomProperty("ACHIEVEMENT_ICON"):WaitForObject():SetImage(achievement.icon)
+    ACHIEVEMENTS_DETAILS_UI:GetCustomProperty("TITLE"):WaitForObject().text = achievement.name
+    ACHIEVEMENTS_DETAILS_UI:GetCustomProperty("DESCRIPTION"):WaitForObject().text = achievement.description
+    ACHIEVEMENTS_DETAILS_UI.visibility = Visibility.FORCE_ON
+end
+
+local function OnDetailsUnhover(button)
+    if not button.clientUserData.detailPanel then
+        return
+    end
+    ACHIEVEMENTS_DETAILS_UI.visibility = Visibility.FORCE_OFF
+end
+
+local function BuildAchievementInfoPanel()
+    local count = 0
+    for _, achievement in pairs(ACH_API.CheckUnlockedAchievements(LOCAL_PLAYER)) do
+
+        --#TODO Needs to be changed to how many achievements should show in the end round panel
+        if count >= 6 then
+            break
+        end
+        local achievementPanel = World.SpawnAsset(AchievementPanelTemplate, {parent = ACHIEVEMENT_PANEL})
+        local iconBG = achievementPanel:GetCustomProperty("ACHIEVEMENT_ICON_BG"):WaitForObject()
+        local icon = achievementPanel:GetCustomProperty("ACHIEVEMENT_ICON"):WaitForObject()
+        local name = achievementPanel:GetCustomProperty("ACHIEVEMENT_NAME"):WaitForObject()
+        --local button = achievementPanel:GetCustomProperty("BUTTON"):WaitForObject()
+
+        --button.clientUserData.detailPanel = achievementPanel:GetCustomProperty("ACHIEVEMENTS_DETAILS_UI"):WaitForObject()
+        --button.clientUserData.achievement = achievement
+
+        --listeners[#listeners + 1] = button.hoveredEvent:Connect(OnDetailsHover)
+        --listeners[#listeners + 1] = button.unhoveredEvent:Connect(OnDetailsUnhover)
+
+        if (iconBG) then
+            iconBG:SetImage(achievement.iconBG)
+        end
+        icon:SetImage(achievement.icon)
+
+        name.text = achievement.name
+
+        if count < 6 then
+            achievementPanel.x = count * 125 --#TODO Needs to be changed
+            count = count + 1
+        else
+            achievementPanel.x = (count - 5) * 130 --#TODO Needs to be changed
+            achievementPanel.y = 150
+            count = count + 1
+        end
+    end
+    --ACHIEVEMENTS_DETAILS_UI.visibility = Visibility.FORCE_OFF
+end
+
+local function AnimateNotification(id)
+    NOTIFICATION_ICON:SetImage(ACH_API.GetAchievementIcon(id))
+    NOTIFICATION_ICON_BG:SetImage(ACH_API.GetAchievementIconBG(id))
+    ACHIEVEMENT_NAME_TEXT.text = (ACH_API.GetAchievementName(id))
+    EaseUI.EaseX(NOTIFICATION, 10, 1, EaseUI.EasingEquation.BACK, EaseUI.EasingDirection.OUT)
+    Task.Wait(0.5)
+    World.SpawnAsset(SFX)
+    Task.Wait(5)
+    EaseUI.EaseX(NOTIFICATION, 400, 1, EaseUI.EasingEquation.SINE, EaseUI.EasingDirection.OUT)
+    Task.Wait(1)
+end
+------------------------------------------------------------------------------------------------------------------------
+-- GLOBAL FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------
+function OnResourceChanged(player, resName, resAmt)
+    if player == LOCAL_PLAYER and IsAchievement(resName) and resAmt == ACH_API.GetAchievementRequired(resName) then
+        achievementQueue[#achievementQueue + 1] = resName
+    elseif player == LOCAL_PLAYER and IsAchievement(resName) and resAmt == 1 then
+    --#TODO Achievement Claimed
+    --World.SpawnAsset(SFX_Achievement)
+    end
+end
+
+function Int()
+    ACH_API.RegisterAchievements(ACHIEVEMENT_LIST)
+    Task.Wait()
+    BuildIdTable()
+    ClearAchievements()
+    if ABGS and ABGS.GetGameState() == ABGS.GAME_STATE_ROUND then
+        shouldShow = true
+        NOTIFICATION.visibility = Visibility.FORCE_ON
+    end
+end
+
+function OnGameStateChanged(oldState, newState, stateHasDuration, stateEndTime) --
+    if newState == ABGS.GAME_STATE_LOBBY then
+        --ACHIEVEMENTS_DETAILS_UI.visibility = Visibility.FORCE_OFF
+    elseif newState ~= ABGS.GAME_STATE_ROUND then
+        shouldShow = true
+        NOTIFICATION.visibility = Visibility.FORCE_ON
+    else
+        shouldShow = true
+        NOTIFICATION.visibility = Visibility.FORCE_ON
+    end
+    if newState == ABGS.GAME_STATE_PLAYER_SHOWCASE then
+        BuildAchievementInfoPanel()
+    else
+        ClearAchievements()
+        ClearListeners(listeners)
+    end
+end
+
+function Tick()
+    if shouldShow and #achievementQueue > 0 then
+        for _, id in ipairs(achievementQueue) do
+            AnimateNotification(id)
+        end
+        achievementQueue = {}
+    end
+end
+
+Int()
+scriptListeners[#scriptListeners + 1] = LOCAL_PLAYER.resourceChangedEvent:Connect(OnResourceChanged)
+scriptListeners[#scriptListeners + 1] = Events.Connect("GameStateChanged", OnGameStateChanged)
+
+scriptListeners[#scriptListeners + 1] =
+    script.destroyEvent:Connect(
+    function()
+        ClearListeners(scriptListeners)
+    end
+)
