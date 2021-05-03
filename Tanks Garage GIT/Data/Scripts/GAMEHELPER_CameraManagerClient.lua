e282dfea0327518e65466a80b0feaf16a910fc8a
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2021-05-02 17:37:29 -0700

    Enabled click-and-drag and zoom for default camera in the garage.
    Former-commit-id: e6f5489fd3b38efc1be7dad869ce5f14aa8a13a1

--- Tanks Garage GIT/Data/Scripts/GAMEHELPER_CameraManagerClient.lua
+++ Tanks Garage GIT/Data/Scripts/GAMEHELPER_CameraManagerClient.lua
@@ -1,6 +1,17 @@
 local cameras = script:GetCustomProperty("Cameras"):WaitForObject()
+local defaultCameraPivot = script:GetCustomProperty("DEFAULT_MENU_CameraPivot"):WaitForObject()
 
 local oldCamera = nil
+local bindingPressedListener = nil
+local bindingReleasedListener = nil
+local trackDrag = false
+
+local startCursorLocation = nil
+local cursorLocationDifference = nil
+local newCursorLocation = nil
+
+local newRotation = Rotation.ZERO
+local currentRotation = Rotation.ZERO
 
 local localPlayer = Game.GetLocalPlayer()
 
@@ -12,6 +23,8 @@ function CameraTransition(newState)
 		
 		if selectedCamera then
 		
+			ToggleClickDrag(selectedCamera)
+			
 			oldCamera = selectedCamera
 		
 			localPlayer:SetOverrideCamera(selectedCamera)
@@ -23,11 +36,13 @@ function CameraTransition(newState)
 	end 
 	
 	if oldCamera and oldCamera:IsA("Camera") and selectedCamera and selectedCamera:IsA("Camera") then
-	
+
+		ToggleClickDrag(selectedCamera)
+		
 		Events.Broadcast("CAMERA_TRANSITION", oldCamera, selectedCamera)
 		
 		oldCamera = selectedCamera
-		
+				
 	end
 
 end
@@ -48,6 +63,65 @@ function DisableOverrideCamera()
 		
 	end
 	
+end
+
+function CheckClickAndDrag(player, binding)
+
+	if binding == "ability_secondary" then
+		if localPlayer:IsBindingPressed(binding) then
+			startCursorLocation = UI.GetCursorPosition()
+			newRotation = defaultCameraPivot:GetRotation()
+			trackDrag = true
+		else 
+			trackDrag = false
+		end
+	end
+
+end
+
+function ToggleClickDrag(selectedCamera)
+
+	if selectedCamera.name == "DEFAULT_MENU" and not bindingPressedListener then
+	
+		bindingPressedListener = localPlayer.bindingPressedEvent:Connect(CheckClickAndDrag)
+		bindingReleasedListener = localPlayer.bindingReleasedEvent:Connect(CheckClickAndDrag)
+		
+		selectedCamera.currentDistance = 0
+	
+	elseif bindingPressedListener then
+	
+		bindingPressedListener:Disconnect()
+		bindingPressedListener = nil
+		
+		bindingReleasedListener:Disconnect()
+		bindingReleasedListener = nil
+		
+		defaultCameraPivot:RotateTo(Rotation.ZERO, 1, true)
+	
+	end
+
+end
+
+function Tick()
+
+	if not trackDrag then
+		return
+	end
+	
+	
+	cursorLocationDifference = (startCursorLocation - UI.GetCursorPosition()) * 0.5
+	
+	if cursorLocationDifference.y + newRotation.y < -45 then
+		cursorLocationDifference.y = -45 - newRotation.y
+	end
+	
+	if cursorLocationDifference.y + newRotation.y > 10 then
+		cursorLocationDifference.y = 10 - newRotation.y
+	end
+		
+	defaultCameraPivot:RotateTo(newRotation + Rotation.New(0, cursorLocationDifference.y, -cursorLocationDifference.x), 0.1, true)
+	print(defaultCameraPivot:GetRotation().y)	
+
 end
 
 Events.Connect("ENABLE_GARAGE_COMPONENT", CameraTransition)
