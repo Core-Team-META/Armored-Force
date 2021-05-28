60a0abea9fdc44bb5d0f97c90c54d2d32dffad50
datonare <datbow85@gmail.com>
2021-05-28 12:25:01 -0400

    Changes to garage
    - Removed "Go to battle" dev UI
    - Changed Choose Mode dropdown to be for gamemode and not map
    - Added Random Battle to game mode and have it selected by default
    - Did a little face lift on the player resource displayer
    - Added an indicator when you are queued for a game
    
    
    Former-commit-id: 4bb62df8c1b26c8915ea8dc8883db52c471d3e25

--- Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
+++ Tanks Garage GIT/Data/Scripts/GAMESTATE_GARAGE_DefaultViewServer.lua
@@ -1,4 +1,3 @@
-
 local map = {}
 
 map["LastTeam"] = {"55d07c/fields-lts", "82b567/desert-lts", "3f6963/tundra-lts"}
@@ -7,12 +6,12 @@ map["Frontline"] = {"18e141/fields-frt", "bb70a8/desert-frt", "707106/tundra-frt
 -- Individual maps
 map["Fields"] = {"18e141/fields-frt", "55d07c/fields-lts"}
 map["Desert"] = {"bb70a8/desert-frt", "82b567/desert-lts"}
-map["Tundra"] = {"707106/tundra-frt", "3f6963/tundra-lts"}
+map["Random"] = {"55d07c/fields-lts", "82b567/desert-lts", "3f6963/tundra-lts", "18e141/fields-frt", "bb70a8/desert-frt", "707106/tundra-frt"}
 
 function SendToMap(player, selectedMap)
-	
+
 	for x, m in pairs(map[selectedMap]) do
-		player:TransferToGame(m)
+		player:TransferToGame(m)		
 	end
 
 end
