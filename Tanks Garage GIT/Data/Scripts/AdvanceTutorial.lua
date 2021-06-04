18ab12f684b70c30720fe82ad206540109569c1f
datonare <datbow85@gmail.com>
2021-06-04 12:20:18 -0400

    Tutorial WIP

--- Tanks Garage GIT/Data/Scripts/AdvanceTutorial.lua
+++ Tanks Garage GIT/Data/Scripts/AdvanceTutorial.lua
@@ -5,6 +5,7 @@ local TutorialPhase = script:GetCustomProperty("TutorialPhase")
 local Step = script:GetCustomProperty("Step")
 local EnteredWaypointSFX = script:GetCustomProperty("EnteredWaypointSFX"):WaitForObject()
 local AdvancePhase = script:GetCustomProperty("AdvancePhase")
+local TutorialCompletePopup = script:GetCustomProperty("TutorialCompletePopup")
 
 function OnOverlap(trigger, other)
 	if(other.driver) then
@@ -13,6 +14,8 @@ function OnOverlap(trigger, other)
 			EnteredWaypointSFX:Play()
 			other.driver.clientUserData.tutorial1 = Step + 1
 			if(AdvancePhase) then
+				local panel = World.SpawnAsset(TutorialCompletePopup, {parent = World.FindObjectByName("Tutorial UI")})
+				panel.lifeSpan = 3
 				Events.BroadcastToServer("AdvanceTutorial", API_Tutorial.TutorialPhase.TargetPractice)
 			end
 		end
