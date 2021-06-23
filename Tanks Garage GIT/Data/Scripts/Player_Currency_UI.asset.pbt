807e1f17e99b691a330615a656f235e3d702fc6a
Brent Dunn <brent075@gmail.com>
2021-06-23 15:22:14 -0700

    Garage [Added] Currency display logic top right corner
    Former-commit-id: e0fc89ca2a08fb2fb02d6b50226e6c453ac5a002

---
+++ Tanks Garage GIT/Data/Scripts/Player_Currency_UI.asset.pbt
@@ -0,0 +1,8 @@
+Assets {
+  Id: 15380404744105278529
+  Name: "Player_Currency_UI"
+  PlatformAssetType: 3
+  TextAsset {
+  }
+  SerializationVersion: 89
+}
---
+++ Tanks Garage GIT/Data/Scripts/Player_Currency_UI.lua
@@ -0,0 +1,40 @@
+--#FIXME This can be moved to a UI Manager in the future
+local CONST = require(script:GetCustomProperty("MetaAbilityProgressionConstants_API"))
+local UTIL = require(script:GetCustomProperty("MetaAbilityProgressionUTIL_API"))
+
+local LOCAL_PLAYER = Game.GetLocalPlayer()
+local AMOUNT_SILVER = script:GetCustomProperty("AMOUNT_SILVER"):WaitForObject()
+local AMOUNT_GOLD = script:GetCustomProperty("AMOUNT_GOLD"):WaitForObject()
+local AMOUNT_RP = script:GetCustomProperty("AMOUNT_RP"):WaitForObject()
+
+local listener
+local currencyInfo = {}
+
+currencyInfo[CONST.SILVER] = AMOUNT_SILVER
+currencyInfo[CONST.GOLD] = AMOUNT_GOLD
+currencyInfo[CONST.FREERP] = AMOUNT_RP
+
+function Init()
+    for _, key in ipairs(CONST.CURRENCY) do
+        local value = LOCAL_PLAYER:GetResource(key)
+        if currencyInfo[key] then
+            currencyInfo[key].text = tostring(value)
+            for _, child in ipairs(currencyInfo[key]:GetChildren()) do
+                child.text = tostring(value)
+            end
+        end
+    end
+end
+
+function OnResourceChanged(player, key, value)
+    print(key, value, currencyInfo[key])
+    if currencyInfo[key] then
+        currencyInfo[key].text = tostring(value)
+        for _, child in ipairs(currencyInfo[key]:GetChildren()) do
+            child.text = tostring(value)
+        end
+    end
+end
+
+listener = LOCAL_PLAYER.resourceChangedEvent:Connect(OnResourceChanged)
+Init()
