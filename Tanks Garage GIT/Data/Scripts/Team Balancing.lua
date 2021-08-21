dca384056b23b89429426c4478ac0e5e336dc6f1
Brent Dunn <brent075@gmail.com>
2021-08-20 17:13:10 -0700

    [Map 1] Added: Team Balancer

---
+++ Tanks Garage GIT/Data/Scripts/Team Balancing.lua
@@ -0,0 +1,241 @@
+local CONST = require(script:GetCustomProperty("MetaAbilityProgressionConstants_API"))
+
+local GAME_STATE = script:GetCustomProperty("GAMESTATE_MainGameStateManagerServer"):WaitForObject()
+local GAME_STATE_LOBBY = script:GetCustomProperty("GAMESTATE_MatchLobbyServer"):WaitForObject()
+
+
+local DEBUG_SAME_TEAM = script:GetCustomProperty("DebugSameTeam")
+
+while not _G.TANK_DATA do
+	Task.Wait()
+end
+
+local tankData = _G.TANK_DATA
+
+local BASE_VALUE_PER_PLAYER = 1
+local TOTAL_CLASS_VALUE_EXPONENT = 0.45 -- Higher value means that ability leveling is ever-more powerful
+local TOTAL_CLASS_VALUE_COEFFICIENT = 6 -- Higher value means players with progression are considered much more powerful
+local WIN_RATE_MIN = 0.2
+local WIN_RATE_MAX = 0.8
+local WIN_RATE_EXPONENT = 1
+local WIN_RATE_COEFFICIENT = 500
+local LOBBY_REBALANCE_TIME = 8
+
+local function IsLobby()
+	return GAME_STATE:GetCustomProperty("GameState") == "LOBBYSTATE"
+end
+
+function ComputePlayerValue(player)
+	-- Return the cached value
+	if player.serverUserData.balanceValue then
+		return player.serverUserData.balanceValue
+	end
+
+	-- Each player brings a base value to the team
+	local value = BASE_VALUE_PER_PLAYER
+
+	local tankId = player:GetResource(CONST.GetEquippedTankResource())
+	local tankStrength = tankData[tankId].tier
+	tankStrength = tankStrength ^ TOTAL_CLASS_VALUE_EXPONENT
+	tankStrength = tankStrength * TOTAL_CLASS_VALUE_COEFFICIENT
+	value = value + tankStrength
+
+	player.serverUserData.balanceValue = value
+
+	print(
+		"[Balance] Player " ..
+			player.name ..
+				", tankId = " ..
+					tostring(tankId) ..
+						"->" ..
+							tostring(tankStrength) .. --", winRateValue = " .. tostring(weightedWinRate) .. "->" .. tostring(winRateValue) ..
+								", totalValue = " .. tostring(value)
+	)
+
+	return value
+end
+
+function ClearCachedPlayerValues()
+	for _, player in ipairs(Game.GetPlayers()) do
+		player.serverUserData.balanceValue = nil
+	end
+end
+
+function ComputeTeamValue(teamOfPlayers)
+	local value = 0
+	for _, player in ipairs(teamOfPlayers) do
+		value = value + ComputePlayerValue(player)
+	end
+	return value
+end
+
+function SwitchTeam(player)
+	if player.team == 1 then
+		player.team = 2
+	else
+		player.team = 1
+	end
+
+	player:Spawn()
+
+	--print(player.name.." was switched to team "..tostring(player.team))
+end
+
+function OfferSwitchChoice()
+	--[[
+		TODO
+		
+		The idea here would be to pop a UI offering players a choice, in the middle of
+		a round if they want to balance teams. We don't want to do that automatically
+		in the middle of a round.
+	--]]
+end
+
+function ApplyTeamChanges(team1, team2)
+	for _, player in ipairs(team1) do
+		if player.team ~= 1 then
+			SwitchTeam(player)
+		end
+	end
+	for _, player in ipairs(team2) do
+		if player.team ~= 2 then
+			SwitchTeam(player)
+		end
+	end
+end
+
+function DoRebalance(playerToIgnore)
+	--print("DoRebalance()")
+
+	local team1 = Game.GetPlayers({includeTeams = 1, ignorePlayers = playerToIgnore})
+	local team2 = Game.GetPlayers({includeTeams = 2, ignorePlayers = playerToIgnore})
+
+	if #team1 + #team2 <= 1 then
+		return
+	end
+
+	-- First, balance amount of players on each time to have no more than 1 extra player
+	while #team1 > #team2 do
+		local player = team1[1]
+		table.remove(team1, 1)
+		table.insert(team2, player)
+	end
+	while #team2 > #team1 do
+		local player = team2[1]
+		table.remove(team2, 1)
+		table.insert(team1, player)
+	end
+
+	-- If each team has one player it's done
+	if #team1 == 1 and #team2 == 1 then
+		ApplyTeamChanges(team1, team2)
+		return
+	end
+
+	-- Swap players until value delta is minimized
+	local value1 = ComputeTeamValue(team1)
+	local value2 = ComputeTeamValue(team2)
+
+	if #team1 > #team2 then
+		value1 = value1 + (100 / #team1)
+	elseif #team2 > #team1 then
+		value2 = value2 + (100 / #team2)
+	end
+
+	local bestDelta = math.abs(value1 / #team1 - value2 / #team2)
+
+	local i = 0
+	while i < 64 do
+		i = i + 1
+
+		-- Remove a random player from each team
+		local randomIndex = math.random(1, #team1)
+		local player1 = team1[randomIndex]
+		table.remove(team1, randomIndex)
+
+		randomIndex = math.random(1, #team2)
+		local player2 = team2[randomIndex]
+		table.remove(team2, randomIndex)
+
+		-- Swap them
+		table.insert(team1, player2)
+		table.insert(team2, player1)
+
+		-- Recalculate value to see if this is better
+		local v1 = value1 - player1.serverUserData.balanceValue + player2.serverUserData.balanceValue
+		local v2 = value2 - player2.serverUserData.balanceValue + player1.serverUserData.balanceValue
+
+		local newDelta = math.abs(v1 / #team1 - v2 / #team2)
+
+		if bestDelta > newDelta then
+			bestDelta = newDelta
+			value1 = v1
+			value2 = v2
+		else
+			-- Revert the swap
+			table.remove(team1, #team1)
+			table.remove(team2, #team2)
+			table.insert(team1, player1)
+			table.insert(team2, player2)
+		end
+	end
+
+	-- Apply any team switching
+	ApplyTeamChanges(team1, team2)
+end
+
+function OnPlayerJoin(player)
+	if DEBUG_SAME_TEAM then
+		player.team = 1
+		return
+	end
+
+	Task.Wait(0.15)
+	if not Object.IsValid(player) then
+		return
+	end
+	if IsLobby() then
+		local team1 = Game.GetPlayers({includeTeams = 1, ignorePlayers = player})
+		local team2 = Game.GetPlayers({includeTeams = 2, ignorePlayers = player})
+
+		if player.team == 1 and #team1 > #team2 then
+			-- Player was put on the wrong team and now the player count is imbalanced
+			SwitchTeam(player)
+		elseif player.team == 2 and #team2 > #team1 then
+			-- Player was put on the wrong team and now the player count is imbalanced
+			SwitchTeam(player)
+		elseif #team1 == #team2 then
+			-- If player count was even, put the player on the team that's losing
+			local score1 = Game.GetTeamScore(1)
+			local score2 = Game.GetTeamScore(2)
+
+			if (score1 > score2 and player.team == 1) or (score2 > score1 and player.team == 2) then
+				SwitchTeam(player)
+			end
+		end
+	end
+end
+
+function OnPlayerLeft(playerToIgnore)
+	--print("Player left")
+
+	-- Does the leaving player still appear on list of GetPlayers() ?
+	-- If so, ignore them in the algorithm
+
+	if IsLobby() then
+		DoRebalance(playerToIgnore)
+	end
+end
+
+function OnLobbyTimerChanged(objecct, string)
+
+	if GAME_STATE_LOBBY:GetCustomProperty(string) == 2 then
+		DoRebalance()
+	end
+end
+
+-- handler params: CoreObject_owner, string_propertyName
+GAME_STATE_LOBBY.networkedPropertyChangedEvent:Connect(OnLobbyTimerChanged)
+
+Game.playerJoinedEvent:Connect(OnPlayerJoin)
+Game.playerLeftEvent:Connect(OnPlayerLeft)
