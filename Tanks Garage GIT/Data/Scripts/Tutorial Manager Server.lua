a9360dc58e0833241605498f7137e0b26b0f9185
datonare <datbow85@gmail.com>
2021-08-31 13:39:10 -0400

    Deploying to the shooting range will now properly start the tutorial

--- Tanks Garage GIT/Data/Scripts/Tutorial Manager Server.lua
+++ Tanks Garage GIT/Data/Scripts/Tutorial Manager Server.lua
@@ -1,7 +1,8 @@
 local API_Tutorial = require(script:GetCustomProperty("API_Tutorial"))
 
 function AdvanceTutorial(player, phase, giveRewards)
-	print(giveRewards)
+	if player:GetResource(API_Tutorial.GetTutorialResource()) >= phase then return end
+
 	if(phase == API_Tutorial.TutorialPhase.Completed and giveRewards) then
 		player:AddResource("Silver", 5000)
 		player:AddResource("Free RP", 5000)
