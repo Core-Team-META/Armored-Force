f6b3e049d7c4cbc0054eed481c7124dcffec6b23
datonare <datbow85@gmail.com>
2021-09-13 22:39:36 -0400

    Fixes to tutorial rewards and removed waypoints from map 1 (they are not used)

--- Tanks Garage GIT/Data/Scripts/Tutorial Manager Server.lua
+++ Tanks Garage GIT/Data/Scripts/Tutorial Manager Server.lua
@@ -4,11 +4,11 @@ function AdvanceTutorial(player, phase, giveRewards)
 --print("Request to advance tutorial to: " .. tostring(phase))
 	if player:GetResource(API_Tutorial.GetTutorialResource()) >= phase and phase ~= API_Tutorial.TutorialPhase.None then return end
 
-	if(phase == API_Tutorial.TutorialPhase.Completed and giveRewards) then
+	if(phase == API_Tutorial.TutorialPhase.Upgrade and giveRewards) then
 		if player:GetResource(API_Tutorial.GetTutorialRewardResource()) < phase then
 --print("Giving rewards")
 			player:AddResource("Silver", 5000)
-			player:AddResource("Free RP", 5000)
+			player:AddResource("Free XP", 5000)
 			player:AddResource("Gold", 100)
 		end
 	elseif(phase < API_Tutorial.TutorialPhase.Completed and giveRewards) then
