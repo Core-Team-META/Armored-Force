a11f74e4513f07064a88eab2d0d57e356e3e4c85
PatrickCosim <55603848+PatrickCosim@users.noreply.github.com>
2021-10-06 16:21:19 -0700

    Added a list of all of the stat names inside the Const_UpgradeTemplate.
    Former-commit-id: f52247c4b4013824487ca6db04540ab26ea9a4e1

---
+++ Tanks Garage GIT/Data/Scripts/Const_UpgradeTemplate.lua
@@ -0,0 +1,32 @@
+--[[
+STAT NAMES:
+	
+	Base tank stats:
+	
+	AIM - increase turret speed (additive)
+	DAMAGE - increase damage per shot (additive)
+	HITPOINTS - increase hitpoints (additive)
+	SPEED - increase top speed (additive)
+	ACCELERATION - increase accleration (additive)
+	TURNING - increase turning speed (additive)
+	
+	
+	Crew upgrade effects:
+	
+	RELOADBOOST - reduce time to reload (percentage)
+	AUTOTREAD - reduce time to automatically repair tread (additive)
+	AUTOEXTINGUISHER - reduce time to automatically put fire out (additive)
+	AUTOREPAIR - reduce time to automatically repair turret (additive)
+	ZOOM - increase sniper view zoom (percentage)
+	FREETREAD - start with an extra amount of tread repair kits (additive)
+	FREEEXTINGUISHER - start with an extra amount of fire extinguishers (additive)
+	TURNINGBOOST - increase turning speed by a percentage, and this percentage is based on the base stats without upgrades (percentage)
+	FIRERESISTANCE - reduce damage taken from being on fire (percentage)
+	SPEEDBOOST - increase top speed by a percentage, and this percentage is based on the base stats without upgrades (percentage)
+	PARTSGAIN - increase amount of parts earned (percentage)
+	SILVERGAIN - increase amount of silver earned (percentage)
+	AIMBOOST - increase turret speed (additive)
+	PROJECTILESPEED - increase projectile speed (percentage)
+	SPOTTING - increase spotting range (percentage)
+	
+]]--
